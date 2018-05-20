5957fb622b1b1fc358aac955743103859cca56d9
demi guo <demiguo98@gmail.com>
2018-05-20 10:28:48 +0800

    data generator

--- research/maskgan/newdata/copy_data.py
+++ research/maskgan/newdata/copy_data.py
@@ -1,15 +1,16 @@
 import numpy as np
 from IPython import embed
 from sets import Set
+from tqdm import tqdm
 GLOVE_PATH="/home/ec2-user/ScrambleTests/dataset/GloVe/glove.840B.300d.txt"
 
 PTB_PATH="/tmp/ptb-copy/vocab.txt"
 OUT_PATH="/tmp/ptb-v4/"
 
+L = 10
 T = 80
-min_len = 3
-max_len=9
 N = 42000
+# 2L + T
 
 
 def get_words(n=100):
@@ -25,24 +26,31 @@ def get_words(n=100):
     random.shuffle(words)
     assert n+2 <= len(words)
     return words[:n], words[n], words[n+1]
-    
+
 
 a07, a8, a9 = get_words(100)
 
-#output = open("copy_data_ptb_T%d_min%d_v3.txt" % (T,min_len), "w") 
-output = open("/tmp/ptb-copy-v4/ptb.valid.txt", "w")
-num_words = len(words)
-for i in range(N):
-    line = []
-    for j in range(10):
-        wid = np.random.randint(0, high=num_words-1)
-        line.append(words[wid])
-    for j in range(T-1):
-        line.append(a8)
-    line.append(a9)
-    for j in range(10):
-        line.append(line[j])
-    line_str = " ".join(line)
-    output.write(" " + line_str + " \n")    
-output.close()
+import numpy as np
+def generate_line():
+    seq = np.random.choice(a07, L, replace=True)
+    input_seq = seq.tolist()
+    input_seq.extend([a8] * (T-1))
+    input_seq.append(a9)
+    input_seq.extend([a8] * L)
+
+    output_seq = [a8] * (T + L)
+    output_seq.extend(seq.tolist())
+
+    # sanity check
+    assert len(input_seq) == len(output_seq)
+
+    return " ".join(input_seq) + " " + " ".join(output_seq)
+
+for output_file in ["ptb.train.txt", "ptb.valid.txt", "ptb.test.txt"]:
+    # NB(demi): probability of overlap is little
+    f = open(output_file)
+    for i in tqdm(range(N), desc=output_file):
+        line = generate_line()
+        f.write(" " + line + " \n")
+    f.close()
 
